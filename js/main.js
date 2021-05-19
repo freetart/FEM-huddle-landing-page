0408ce95bb60090fc5c9f2eeb92b05a63ec49db4
habib <habibdev96@gmail.com>
2021-05-19 01:48:29 -0600

    created sidebar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 "use strict";
+
+const sidebar = () => {
+  const sidebarOpenBtn = document.querySelector(".nav__hamburger--open");
+  const sidebarCloseBtn = document.querySelector(".nav__hamburger--close");
+  const sidebarEl = document.querySelector(".nav__sidebar");
+  const overlay = document.querySelector(".overlay");
+
+  // show sidebar
+  sidebarOpenBtn.addEventListener("click", () => {
+    sidebarEl.classList.add("show");
+    overlay.classList.remove("hidden");
+  });
+
+  // close sidebar
+  sidebarCloseBtn.addEventListener("click", () => {
+    sidebarEl.classList.remove("show");
+    overlay.classList.add("hidden");
+  });
+
+  // close sidebar via overlay
+  overlay.addEventListener("click", () => {
+    sidebarEl.classList.remove("show");
+    overlay.classList.add("hidden");
+  });
+};
+sidebar();
